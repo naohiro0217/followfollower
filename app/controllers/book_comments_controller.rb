c43981621dcbda964d38412555cc45bef9d0ee2a
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-11-05 07:18:37 +0000

    komento hidouki

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -9,7 +9,7 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    @books=Book.find(params[:book_id])
+    @book=Book.find(params[:book_id])
     @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
     @book_comment.destroy
     @book_comment = BookComment.new
